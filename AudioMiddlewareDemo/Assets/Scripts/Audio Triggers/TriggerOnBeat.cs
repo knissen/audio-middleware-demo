261871cabc7143b2748d0e6dfa4967c4e371a5c5
kylenissen <nissenkyle@gmail.com>
2021-02-02 23:07:42 -0700

    Adding a bunch of audio tracks and sfx. Creating Trigger scripts for things on beat and from markers.

---
+++ AudioMiddlewareDemo/Assets/Scripts/Audio Triggers/TriggerOnBeat.cs
@@ -0,0 +1,109 @@
+﻿using FMODUnity;
+using System;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerOnBeat : MonoBehaviour
+{
+    // Variables that are modified in the callback need to be part of a seperate class.
+    // This class needs to be 'blittable' otherwise it can't be pinned in memory.
+    [StructLayout(LayoutKind.Sequential)]
+    class TimelineInfo
+    {
+        public int currentMusicBar = 0;
+        public int currentBeat = 0;
+        public FMOD.StringWrapper lastMarker = new FMOD.StringWrapper();
+    }
+
+    [SerializeField] [EventRef] private string _musicEventRef = "";
+
+    [Header("Events")]
+    public UnityEvent actionOnOne;
+
+    private TimelineInfo _timelineInfo;
+    private GCHandle _timelineHandle;
+
+    private FMOD.Studio.EVENT_CALLBACK _beatCallback;
+    private FMOD.Studio.EventInstance _musicInstance;
+
+    void Start()
+    {
+        _timelineInfo = new TimelineInfo();
+
+        // Explicitly create the delegate object and assign it to a member so it doesn't get freed
+        // by the garbage collected while it's being used
+        _beatCallback = new FMOD.Studio.EVENT_CALLBACK(BeatEventCallback);
+
+        _musicInstance = FMODUnity.RuntimeManager.CreateInstance(_musicEventRef);
+
+        // Pin the class that will store the data modified during the callback
+        _timelineHandle = GCHandle.Alloc(_timelineInfo, GCHandleType.Pinned);
+        // Pass the object through the userdata of the instance
+        _musicInstance.setUserData(GCHandle.ToIntPtr(_timelineHandle));
+
+        _musicInstance.setCallback(_beatCallback, FMOD.Studio.EVENT_CALLBACK_TYPE.TIMELINE_BEAT | FMOD.Studio.EVENT_CALLBACK_TYPE.TIMELINE_MARKER);
+        _musicInstance.start();
+    }
+
+    private void Update()
+    {
+        if (_timelineInfo.currentBeat != -1)
+        {
+            actionOnOne?.Invoke();
+
+            _timelineInfo.currentBeat = -1;
+        }
+    }
+
+    void OnDestroy()
+    {
+        _musicInstance.setUserData(IntPtr.Zero);
+        _musicInstance.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+        _musicInstance.release();
+        _timelineHandle.Free();
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Box(String.Format("Current Beat = {0}, Current Bar = {1}, Last Marker = {2}", _timelineInfo.currentBeat, _timelineInfo.currentMusicBar, (string)_timelineInfo.lastMarker));
+    }
+
+    [AOT.MonoPInvokeCallback(typeof(FMOD.Studio.EVENT_CALLBACK))]
+    static FMOD.RESULT BeatEventCallback(FMOD.Studio.EVENT_CALLBACK_TYPE type, IntPtr instancePtr, IntPtr parameterPtr)
+    {
+        FMOD.Studio.EventInstance instance = new FMOD.Studio.EventInstance(instancePtr);
+
+        // Retrieve the user data
+        IntPtr timelineInfoPtr;
+        FMOD.RESULT result = instance.getUserData(out timelineInfoPtr);
+        if (result != FMOD.RESULT.OK)
+        {
+            Debug.LogError("Timeline Callback error: " + result);
+        }
+        else if (timelineInfoPtr != IntPtr.Zero)
+        {
+            // Get the object to store beat and marker details
+            GCHandle timelineHandle = GCHandle.FromIntPtr(timelineInfoPtr);
+            TimelineInfo timelineInfo = (TimelineInfo)timelineHandle.Target;
+
+            switch (type)
+            {
+                case FMOD.Studio.EVENT_CALLBACK_TYPE.TIMELINE_BEAT:
+                    {
+                        var parameter = (FMOD.Studio.TIMELINE_BEAT_PROPERTIES)Marshal.PtrToStructure(parameterPtr, typeof(FMOD.Studio.TIMELINE_BEAT_PROPERTIES));
+                        timelineInfo.currentMusicBar = parameter.bar;
+                        timelineInfo.currentBeat = parameter.beat;
+                    }
+                    break;
+                case FMOD.Studio.EVENT_CALLBACK_TYPE.TIMELINE_MARKER:
+                    {
+                        var parameter = (FMOD.Studio.TIMELINE_MARKER_PROPERTIES)Marshal.PtrToStructure(parameterPtr, typeof(FMOD.Studio.TIMELINE_MARKER_PROPERTIES));
+                        timelineInfo.lastMarker = parameter.name;
+                    }
+                    break;
+            }
+        }
+        return FMOD.RESULT.OK;
+    }
+}
---
+++ AudioMiddlewareDemo/Assets/Scripts/Audio Triggers/TriggerOnMarker.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using FMOD;
+using FMOD.Studio;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerOnMarker : MonoBehaviour
+{
+    // Data has to be in its own class to work with pinning
+    [StructLayout(LayoutKind.Sequential)]
+    class MarkerInfo
+    {
+        public StringWrapper lastMarker = new StringWrapper();
+    }
+
+    public UnityEvent actionOnMarker;
+    public FMODUnity.StudioEventEmitter eventEmitter;
+    public string markerName = "Boom";
+
+    private EVENT_CALLBACK _markerCallback;
+    private MarkerInfo _markerInfo;
+    private GCHandle _markerHandle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Keep a reference to the callback so it is not garbage collected
+        _markerCallback = new FMOD.Studio.EVENT_CALLBACK(OnMarkerHit);
+
+        // Local variable to use to hold data from callback
+        _markerInfo = new MarkerInfo();
+
+        // Create a handle to the info variable and pin it so it does not get garbage collected
+        _markerHandle = GCHandle.Alloc(_markerInfo, GCHandleType.Pinned);
+
+        // Pass a pointer to the info variable to the event instance so it can fill it with data in the callback
+        eventEmitter.EventInstance.setUserData(GCHandle.ToIntPtr(_markerHandle));
+        //eventEmitter.EventInstance.setCallback(OnMarkerHit, FMOD.Studio.EVENT_CALLBACK_TYPE.TIMELINE_MARKER);
+    }
+
+    private void Update()
+    {
+        if (!string.IsNullOrEmpty(_markerInfo.lastMarker) && markerName.Equals(_markerInfo.lastMarker))
+        {
+            actionOnMarker.Invoke();
+
+            // Clear the entry so we don't repeat this till next time its set
+            _markerInfo.lastMarker = new StringWrapper();
+        }
+    }
+
+    private void OnDestroy()
+    {
+        // Clear the user data pointer and free the marker handle for garbage collections
+        eventEmitter.EventInstance.setUserData(IntPtr.Zero);
+        _markerHandle.Free();
+    }
+
+    // Attribute indicates this callback will be called from unmanaged code and method must be static
+    [AOT.MonoPInvokeCallback(typeof(EVENT_CALLBACK))]
+    protected static FMOD.RESULT OnMarkerHit(EVENT_CALLBACK_TYPE type, IntPtr instancePtr, IntPtr parameters)
+    {
+        FMOD.Studio.EventInstance instance = new FMOD.Studio.EventInstance(instancePtr);
+
+        var timelineParameters = (TIMELINE_MARKER_PROPERTIES)Marshal.PtrToStructure(parameters, typeof(TIMELINE_MARKER_PROPERTIES));
+
+        string markerName = timelineParameters.name;
+
+        if (markerName.Equals(markerName, StringComparison.CurrentCultureIgnoreCase))
+        {
+            IntPtr markerInfoPtr;
+            FMOD.RESULT result = instance.getUserData(out markerInfoPtr);
+
+            if (result == RESULT.OK && markerInfoPtr != IntPtr.Zero)
+            {
+                GCHandle markerHandle = GCHandle.FromIntPtr(markerInfoPtr);
+                MarkerInfo markerInfo = (MarkerInfo)markerHandle.Target;
+
+                markerInfo.lastMarker = timelineParameters.name;
+            }
+        }
+
+        return RESULT.OK;
+    }
+}
