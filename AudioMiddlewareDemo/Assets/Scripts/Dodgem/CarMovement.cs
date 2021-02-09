8438672dc0026aa0ecb69ac733cd7421f64c2f4b
kylenissen <nissenkyle@gmail.com>
2021-02-08 22:13:33 -0700

    Adding tweening to car movement for Dodg'em

--- AudioMiddlewareDemo/Assets/Scripts/Dodgem/CarMovement.cs
+++ AudioMiddlewareDemo/Assets/Scripts/Dodgem/CarMovement.cs
@@ -1,26 +1,42 @@
-﻿using System.Collections;
+﻿using DG.Tweening;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class CarMovement : MonoBehaviour
 {
+    [SerializeField] private float _moveTime = 0.1f;
+    [SerializeField] private Ease _moveEase = Ease.InElastic;
+
+    private Tween _activeTween;
+
     // Update is called once per frame
     protected void Update()
     {
+        Vector3 nextDelta = Vector3.zero;
+
         if(Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
         {
             if(transform.position.x > 0)
             {
-                transform.position += Vector3.left;
+                nextDelta = Vector3.left;
             }
         }
-
-        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+        else if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
         {
             if (transform.position.x < 4)
             {
-                transform.position += Vector3.right;
+                nextDelta = Vector3.right;
             }
+        }
+
+        if(nextDelta != Vector3.zero)
+        {
+            if (_activeTween != null && _activeTween.active)
+                DOTween.Kill(_activeTween);
+
+            _activeTween = transform.DOMove(transform.position + nextDelta, _moveTime).SetEase(_moveEase);
         }
     }
 }
